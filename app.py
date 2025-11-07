73d567e3c500a7922713ba287fee7f0d9d97e6ca
Jorgepeto <joorge.hierro.f@gmail.com>
2025-11-07 17:38:37 +0100

    .

--- app.py
+++ app.py
@@ -16,41 +16,23 @@ uploaded_file = st.file_uploader(
 if not uploaded_file:
     print('Por favor, sube un archivo válido para continuar.')
 else:
-    modo = st.radio(
-        "¿Qué deseas generar?",
-        ("Solo fotos", "Informe completo"),
-        index=None,
-        help="Selecciona una opción una vez hayas subido el archivo CSV."
-    )
-    
-    df = pd.read_csv(uploaded_file, index_col=0)
-    if modo == "Informe completo":
-        with st.spinner("✍️ Generando el sociograma..."):
+    with st.spinner("✍️ Generando el sociograma..."):
 
-            st.write("Primero, generamos los gráficos...")
-            generar_graficos(df)
+        df = pd.read_csv(uploaded_file, index_col=0)
 
-            st.write("\n Generados todos los gráficos.")
+        st.write("Primero, generamos los gráficos...")
+        generar_graficos(df)
 
-            st.write("Ahora, generamos el informe...")
-            informe = generar_informe()
+        st.write("\n Generados todos los gráficos.")
 
-            st.success("✅ Sociograma generado con éxito!")
+        st.write("Ahora, generamos el informe...")
+        informe = generar_informe()
 
-            st.download_button(
-                label = "📄 Descargar informe PDF",
-                data = informe,
-                file_name = "Informe_Sociograma.pdf",
-                mime = "application/pdf"
-            )
-    else:
-        with st.spinner("🖼️ Generando las fotos..."):
-            fotos = generar_graficos(df)
+        st.success("✅ Sociograma generado con éxito!")
 
-            st.success("✅ Fotos generadas con éxito!")
-            st.download_button(
-                label="📸 Descargar fotos",
-                data=fotos,
-                file_name="Fotos_Sociograma.zip",
-                mime="application/zip"
-            )
+        st.download_button(
+            label = "📄 Descargar informe PDF",
+            data = informe,
+            file_name = "Informe_Sociograma.pdf",
+            mime = "application/pdf"
+        )
